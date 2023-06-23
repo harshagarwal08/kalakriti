0df5201c90c7f2c5525f40005eea2ba577c779c1
Harsh Agarwal <harsh821agarwal@gmail.com>
2023-06-23 15:44:29 +0530

    fix: contact us page

--- pages/contactus.js
+++ pages/contactus.js
@@ -10,32 +10,14 @@ const contactus = () => {
   return (
     <div className={Style.contactus}>
       <div className={Style.contactus_box}>
-        <h1>Contact</h1>
-        <div className={Style.contactus_box_box}>
-          <div className={Style.contactus_box_box_left}>
-            <div className={Style.contactus_box_box_left_item}>
-              <h3>🗺 ADDRESS</h3>
-              <p>
-                Photo booth tattooed prism, portland taiyaki hoodie neutra
-                typewriter
-              </p>
-            </div>
-            <div className={Style.contactus_box_box_left_item}>
-              <h3>💌 EMAIL</h3>
-              <p>nc.example@example.com</p>
-            </div>
-            <div className={Style.contactus_box_box_left_item}>
-              <h3>☎ PHONE</h3>
-              <p>000-123-456-7890</p>
-            </div>
-          </div>
+        <h1>Contact Us</h1>
           <div className={Style.contactus_box_box_right}>
-            <form>
+            <form className={Style.form_container}>
               <div className={formStyle.Form_box_input}>
                 <label htmlFor="name">Full Name</label>
                 <input
                   type="text"
-                  placeholder="shoaib bhai"
+                  placeholder="Enter your name"
                   className={formStyle.Form_box_input_userName}
                 />
               </div>
@@ -45,7 +27,7 @@ const contactus = () => {
                   <div className={formStyle.Form_box_input_box_icon}>
                     <HiOutlineMail />
                   </div>
-                  <input type="text" placeholder="Email*" />
+                  <input type="text" placeholder="Enter your Email" />
                 </div>
               </div>
               <div className={formStyle.Form_box_input}>
@@ -55,7 +37,7 @@ const contactus = () => {
                   id=""
                   cols="30"
                   rows="6"
-                  placeholder="something about yourself in few words"
+                  placeholder="Enter your message"
                 ></textarea>
               </div>
               <Button
@@ -65,7 +47,6 @@ const contactus = () => {
               />
             </form>
           </div>
-        </div>
       </div>
     </div>
   );
